ac19c32ce8fcf79a42fa418fef36e3eb21395ca4
Cybersecurity2000 <melvin.reyes122000@gmail.com>
2025-03-10 23:35:20 +0800

    added searh.tsx

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,73 +1,20 @@
-import { 
-  IonAvatar,
-  IonButtons,
-  IonContent, 
-  IonHeader, 
-  IonImg, 
-  IonItem, 
-  IonLabel, 
-  IonList, 
-  IonMenuButton, 
-  IonPage, 
-  IonSearchbar, 
-  IonTitle, 
-  IonToolbar 
-} from '@ionic/react';
+import React from 'react';
+import { IonSearchbar } from '@ionic/react';
+import { trash } from 'ionicons/icons';
 
-const Search: React.FC = () => {
+function Example() {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot='start'>
-            <IonMenuButton></IonMenuButton>
-          </IonButtons>
-          <IonTitle>Search</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonSearchbar placeholder="Search"></IonSearchbar>
-        <IonList>
-          <IonItem>
-            <IonAvatar slot="start">
-              <IonImg src="/assets/images/tanjiro.png" />
-            </IonAvatar>
-            <IonLabel>
-              <h2>Tanjiro Kamado</h2>
-              <p>Demon Slayer</p>
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonAvatar slot="start">
-              <IonImg src="/assets/images/nezuko.png" />
-            </IonAvatar>
-            <IonLabel>
-              <h2>Nezuko Kamado</h2>
-              <p>Demon</p>
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonAvatar slot="start">
-              <IonImg src="/assets/images/zenitsu.png" />
-            </IonAvatar>
-            <IonLabel>
-              <h2>Zenitsu Agatsuma</h2>
-              <p>Demon Slayer</p>
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonAvatar slot="start">
-              <IonImg src="/assets/images/inosuke.png" />
-            </IonAvatar>
-            <IonLabel>
-              <h2>Inosuke Hashibira</h2>
-              <p>Demon Slayer</p>
-            </IonLabel>
-          </IonItem>
-        </IonList>
-      </IonContent>
-    </IonPage>
+    <>
+      <IonSearchbar showCancelButton="focus" placeholder="Show on Focus"></IonSearchbar>
+      <IonSearchbar showCancelButton="always" placeholder="Always Show"></IonSearchbar>
+      <IonSearchbar showCancelButton="never" placeholder="Never Show"></IonSearchbar>
+      <IonSearchbar
+        showCancelButton="always"
+        cancelButtonText="Custom Cancel"
+        cancelButtonIcon={trash}
+        placeholder="Custom Cancel Button"
+      ></IonSearchbar>
+    </>
   );
-};
-
-export default Search;
+}
+export default Example;
