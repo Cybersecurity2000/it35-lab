719ef131d811ca48dd88da0d017b1cd87fbc562f
Cybersecurity2000 <melvin.reyes122000@gmail.com>
2025-03-11 00:47:59 +0800

    added Search.tsx

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,20 +1,73 @@
-import React from 'react';
-import { IonSearchbar } from '@ionic/react';
-import { trash } from 'ionicons/icons';
+import { 
+  IonAvatar,
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonImg, 
+  IonItem, 
+  IonLabel, 
+  IonList, 
+  IonMenuButton, 
+  IonPage, 
+  IonSearchbar, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
 
-function Example() {
+const Search: React.FC = () => {
   return (
-    <>
-      <IonSearchbar showCancelButton="focus" placeholder="Show on Focus"></IonSearchbar>
-      <IonSearchbar showCancelButton="always" placeholder="Always Show"></IonSearchbar>
-      <IonSearchbar showCancelButton="never" placeholder="Never Show"></IonSearchbar>
-      <IonSearchbar
-        showCancelButton="always"
-        cancelButtonText="Custom Cancel"
-        cancelButtonIcon={trash}
-        placeholder="Custom Cancel Button"
-      ></IonSearchbar>
-    </>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Search</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonSearchbar placeholder="Search"></IonSearchbar>
+        <IonList>
+          <IonItem>
+            <IonAvatar slot="start">
+              <IonImg src="https://gifsec.com/wp-content/uploads/2022/10/tanjiro-gif-12.gif" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Tanjiro Kamado</h2>
+              <p>Demon Slayer</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonAvatar slot="start">
+              <IonImg src="https://www.icegif.com/wp-content/uploads/2022/06/icegif-782.gif" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Nezuko Kamado</h2>
+              <p>Demon</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonAvatar slot="start">
+              <IonImg src="https://media.tenor.com/QZF7CaKs7CwAAAAC/zenitsu-zenitsu-agatsuma.gif" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Zenitsu Agatsuma</h2>
+              <p>Demon Slayer</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonAvatar slot="start">
+              <IonImg src="https://www.icegif.com/wp-content/uploads/2022/10/icegif-1752.gif" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Inosuke Hashibira</h2>
+              <p>Demon Slayer</p>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
   );
-}
-export default Example;
+};
+
+export default Search;
