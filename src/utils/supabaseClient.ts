839a6fca87a05364127ca997133a21c3288751ed
Cybersecurity2000 <melvin.reyes122000@gmail.com>
2025-04-01 18:42:37 +0800

    added supabaseclients

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,7 @@
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = 'https://coorcmzlxazalpklbjak.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkem92bHNibXhrYXV3dXVndW1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3NzM3NjUsImV4cCI6MjA1ODM0OTc2NX0.pg4NWyiqE56jfC9VJOuNXRP9jyKn5dd89Fd3TKeaxgE'; // Replace with your own Supabase API Key
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
